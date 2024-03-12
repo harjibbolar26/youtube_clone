467526df2f9dfacc7146bc9a639d3b7031ce9310
Jibola Paul <harjibbolar26@gmail.com>
2024-03-12 13:03:06 +0100

    new commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "2b58396f6emsh4ab03c2113aab71p1a62efjsn704ce5776aee",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
